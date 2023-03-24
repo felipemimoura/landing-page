692de0dc87809db29942789e96cd589a874c51f0
felipemimoura <felipemimoura@gmail.com>
2023-03-24 07:11:41 -0300

    feat: get logo props

--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -6,40 +6,44 @@ import * as S from './styles'
 
 import { gaEvent } from 'utils/ga'
 import Container from 'components/Container'
+import { LogoProps } from 'types/api'
 
 const onClick = () =>
   gaEvent({ action: 'click', category: 'cta', label: 'hero button' })
 
-const SectionHero = () => (
-  <S.Wrapper>
-    <Container>
-      <Logo />
+type Props = {
+  logo: LogoProps
+}
 
-      <S.Content>
-        <S.TextBlock>
-          <S.Title>React Avançado</S.Title>
-          <S.Description>
-            Crie aplicações reais com NextJS, Strapi, GraphQL e mais!
-          </S.Description>
-          <S.Warning>⚠️ Atenção: Suporte ao curso finalizado!</S.Warning>
-          <S.ButtonWrapper>
-            <Button
-              href="https://www.udemy.com/course/react-avancado/?couponCode=C6DFF6069B10A28BA2BC"
-              onClick={onClick}
-              wide
-            >
-              Comprar
-            </Button>
-          </S.ButtonWrapper>
-        </S.TextBlock>
+const SectionHero = ({ logo }: Props) => {
+  return (
+    <S.Wrapper>
+      <Container>
+        <Logo {...logo} />
 
-        <S.Image
-          src="/img/hero-illustration.svg"
-          alt="Ilustração de um desenvolvedor em frente a um computador com várias linhas de código."
-        />
-      </S.Content>
-    </Container>
-  </S.Wrapper>
-)
+        <S.Content>
+          <S.TextBlock>
+            <S.Title>React Avançado</S.Title>
+            <S.Description>
+              Crie aplicações reais com NextJS, Strapi, GraphQL e mais!
+            </S.Description>
+            <S.Warning>⚠️ Atenção: Suporte ao curso finalizado!</S.Warning>
+            <S.ButtonWrapper>
+              <Button
+                href="https://www.udemy.com/course/react-avancado/?couponCode=C6DFF6069B10A28BA2BC"
+                onClick={onClick}
+                wide
+              >
+                Comprar
+              </Button>
+            </S.ButtonWrapper>
+          </S.TextBlock>
+
+          <S.Image src="/img/hero-illustration.svg" alt="alternativeText" />
+        </S.Content>
+      </Container>
+    </S.Wrapper>
+  )
+}
 
 export default SectionHero
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { GetStaticProps } from 'next'
 
 import SectionHero from 'components/SectionHero'
 import SectionAboutProject from 'components/SectionAboutProject'
@@ -13,21 +14,56 @@ import SectionFaq from 'components/SectionFaq'
 import Footer from 'components/Footer'
 import JsonSchema from 'components/JsonSchema'
 
-const Index = () => (
-  <>
-    <SectionHero />
-    <SectionAboutProject />
-    <SectionTech />
-    <SectionConcepts />
-    <SectionModules />
-    <SectionAgenda />
-    <PricingBox />
-    <SectionAboutUs />
-    <SectionReviews />
-    <SectionFaq />
-    <Footer />
-    <JsonSchema />
-  </>
-)
+import client from 'graphql/client'
+import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
+import { LandingPageProps, LogoProps } from 'types/api'
 
+type Teste = {
+  landingPage: {
+    data: {
+      attributes: {
+        logo: {
+          data: {
+            attributes: LogoProps
+          }
+        }
+      }
+    }
+  }
+}
+
+const Index = ({ logo }: LandingPageProps) => {
+  return (
+    <>
+      <SectionHero logo={logo} />
+      <SectionAboutProject />
+      <SectionTech />
+      <SectionConcepts />
+      <SectionModules />
+      <SectionAgenda />
+      <PricingBox />
+      <SectionAboutUs />
+      <SectionReviews />
+      <SectionFaq />
+      <Footer />
+      <JsonSchema />
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { landingPage } = await client.request<Promise<Teste>>(GET_LANDING_PAGE)
+  const logo = landingPage.data.attributes.logo.data.attributes
+  console.log(logo)
+
+  const teste: LandingPageProps = {
+    logo: logo
+  }
+
+  return {
+    props: {
+      ...teste
+    }
+  }
+}
 export default Index
