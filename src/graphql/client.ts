ef8b6e8ec1052855b4ffd4ab104ef3f5b8b7e20a
felipemimoura <felipemimoura@gmail.com>
2023-03-24 07:11:58 -0300

    feat: add GRAPHQL env

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from 'graphql-request'
 
 //Criação do client graphQL
-const client = new GraphQLClient('http://localhost:1337/graphql')
+const client = new GraphQLClient(process.env.GRAPHQL_HOST)
 
 export default client
