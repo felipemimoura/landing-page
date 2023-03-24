dba4d8ba9dcb44838f012964220f122d7a410c47
felipemimoura <felipemimoura@gmail.com>
2023-03-24 07:41:21 -0300

    feat: chage header section to dynamic values

--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -6,16 +6,18 @@ import * as S from './styles'
 
 import { gaEvent } from 'utils/ga'
 import Container from 'components/Container'
-import { LogoProps } from 'types/api'
+import { HeaderProps, LogoProps } from 'types/api'
+import { getImageUrl } from 'utils/getImageUrl'
 
 const onClick = () =>
   gaEvent({ action: 'click', category: 'cta', label: 'hero button' })
 
 type Props = {
   logo: LogoProps
+  header: HeaderProps
 }
 
-const SectionHero = ({ logo }: Props) => {
+const SectionHero = ({ logo, header }: Props) => {
   return (
     <S.Wrapper>
       <Container>
@@ -23,23 +25,20 @@ const SectionHero = ({ logo }: Props) => {
 
         <S.Content>
           <S.TextBlock>
-            <S.Title>React Avançado</S.Title>
-            <S.Description>
-              Crie aplicações reais com NextJS, Strapi, GraphQL e mais!
-            </S.Description>
+            <S.Title>{header.title}</S.Title>
+            <S.Description>{header.description}</S.Description>
             <S.Warning>⚠️ Atenção: Suporte ao curso finalizado!</S.Warning>
             <S.ButtonWrapper>
-              <Button
-                href="https://www.udemy.com/course/react-avancado/?couponCode=C6DFF6069B10A28BA2BC"
-                onClick={onClick}
-                wide
-              >
-                Comprar
+              <Button href={header.button.url} onClick={onClick} wide>
+                {header.button.label}
               </Button>
             </S.ButtonWrapper>
           </S.TextBlock>
 
-          <S.Image src="/img/hero-illustration.svg" alt="alternativeText" />
+          <S.Image
+            src={getImageUrl(header.image.data.attributes.url)}
+            alt={header.image.data.attributes.alternativeText}
+          />
         </S.Content>
       </Container>
     </S.Wrapper>
